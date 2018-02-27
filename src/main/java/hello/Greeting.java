00ebacbd0132ca10af2ac7951c209ab0080591d0
Nikolaos Michalopoulos <michalopoulos.nk@gmail.com>
2018-02-27 12:41:41 +0200

    Just the form example - no changes in file names - only changed just the two variables in the object greeting from: long id, String comment to: String startingPoint, String endingPoint

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class Greeting {
+
+    private String startingPoint;
+    private String endingPoint;
+
+    //getters and setters
+    public String getStartingPoint() {
+        return startingPoint;
+    }
+
+    public void setStartingPoint(String startingPoint) {
+        this.startingPoint = startingPoint;
+    }
+
+    public String getEndingPoint() {
+        return endingPoint;
+    }
+
+    public void setEndingPoint(String endingPoint) {
+        this.endingPoint = endingPoint;
+    }
+}
